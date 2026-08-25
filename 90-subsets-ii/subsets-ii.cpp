afbe4923d8b3cd2cef390875ea30335dd627706d
Mounika <healermoibus@gmail.com>
2026-08-25 22:35:30 +0530

    Time: 3 ms (31.13%) | Memory: 10.5 MB (50.42%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         int ind=0;
-        sort(nums.begin(), nums.end());
-        vector<int>arr;
+        sort(nums.begin(), nums.end()); /*what is sorting doing here when we already have a for loop down to check duplicates?
+        if we have [1,2,1] , we get [1,2] and [2,1] if we dont sort (for loop doesnt eliminate this) if we sort its [1,1,2]
+        we get [1],[1,1],[1,2] we ownt get [2,1] again like before */      
+        vector<int>arr; 
         vector<vector<int>>ans;
         int n= nums.size();
+
         sub(0,arr,nums,ans,n);
         return ans;
     }
@@ -13,9 +16,9 @@ public:
     {
         if (ind==n)
         {
-            for(auto &v : ans)
+            for(auto &v : ans) //this is where we try to not take duplicates after sorting out the array
             {
-                if(v==arr)
+                if(v==arr) //we arec checking if the subset we push to ans already exists in ans or not
                 {
                     return;
                 }
