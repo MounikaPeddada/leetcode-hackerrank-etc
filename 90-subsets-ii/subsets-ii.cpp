852ead4c8871fad0b8ab9d747144d71ea22d7735
Mounika <healermoibus@gmail.com>
2026-09-05 17:46:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (67.03%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int ind=0;
-        sort(nums.begin(), nums.end()); /*what is sorting doing here when we already have a for loop down to check duplicates?
-        if we have [1,2,1] , we get [1,2] and [2,1] if we dont sort (for loop doesnt eliminate this) if we sort its [1,1,2]
-        we get [1],[1,1],[1,2] we ownt get [2,1] again like before */      
-        vector<int>arr; 
+        //sort the array it simportant or how will it check if nums[i]==nums[i-1]
+        sort(nums.begin(),nums.end());
+        vector<int>arr;
         vector<vector<int>>ans;
-        int n= nums.size();
-
-        sub(0,arr,nums,ans,n);
+        findsubsets(0,arr,nums,ans);
         return ans;
     }
-    void sub(int ind,vector<int>&arr,vector<int>&nums,vector<vector<int>>&ans,int n)
+    void findsubsets(int ind, vector<int>&arr,vector<int>&nums,vector<vector<int>>&ans)
     {
-        if (ind==n)
+        ans.push_back(arr);
+        for(int i=ind;i<nums.size();i++)
         {
-            for(auto &v : ans) //this is where we try to not take duplicates after sorting out the array
-            {
-                if(v==arr) //we arec checking if the subset we push to ans already exists in ans or not
-                {
-                    return;
-                }
-            }
-            ans.push_back(arr);  
-            return;
+            if(i>ind && nums[i]==nums[i-1]) continue;
+            arr.push_back(nums[i]);
+            findsubsets(i+1,arr,nums,ans);
+            arr.pop_back();
         }
-        arr.push_back(nums[ind]);
-        sub(ind+1,arr,nums,ans,n);
-        arr.pop_back();
-        sub(ind+1,arr,nums,ans,n);
     }
 };
