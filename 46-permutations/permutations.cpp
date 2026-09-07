bfbac9665225ee6a1a4269ada888b00987099d67
Mounika <healermoibus@gmail.com>
2026-09-07 10:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (88.77%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int>arr;
         vector<vector<int>>ans;
-        int freq[6];
-        allcombs(arr,ans,freq,nums);
+        allcombs(0,nums,ans);
         return ans;
     }
-    void allcombs(vector<int>&arr,vector<vector<int>>&ans,int freq[],vector<int>&nums)
+    void allcombs(int ind,vector<int>&nums,vector<vector<int>>&ans)
     {
-        //defining base case
-        if(arr.size()==nums.size())
+        if(ind==nums.size()) //base case
         {
-            ans.push_back(arr);
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++)
+        for(int i=ind;i<nums.size();i++)
         {
-            if(!freq[i])
-            {
-                arr.push_back(nums[i]);
-                freq[i]=1;           //mark and push
-                allcombs(arr,ans,freq,nums);
-                freq[i]=0;      //unmarl and pop
-                arr.pop_back();
-            }
+                swap(nums[ind],nums[i]);
+                allcombs(ind+1,nums,ans);
+                swap(nums[ind],nums[i]);
         }
     }
 };
