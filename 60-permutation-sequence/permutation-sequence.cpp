30cbe550b53ec56b2a324d5c25512f297851d5c6
Mounika <healermoibus@gmail.com>
2026-09-12 15:48:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (17.87%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,45 +1,28 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        //brute force approach
-        vector<int>arr;
-        //create nums
+        //optimal solution
+        string s;
+        int fact=1;
         vector<int>nums;
-        //cretae freq
-        vector<int>freq(n,0);
-        string result;
-
-        int count=0;
-
-        for(int i=1;i<=n;i++) nums.push_back(i);
-
-        solve(0,nums,arr,count,n,result,k,freq);
-
-        return result;
-        
-    }
-    void solve(int ind,vector<int>&nums,vector<int>&arr,int& count,int n,string&result,int k,vector<int>&freq)
-    {
-        //base case
-        if(ind==n) 
+        for(int i=1;i<n;i++)
         {
-            count++;
-            if(count==k)
-            {
-                for(auto &num : arr) result+=to_string(num);
-                return;
-            }
+            fact=fact*i;
+            nums.push_back(i);
         }
-        for(int i=0;i<n;i++)
+        nums.push_back(n);
+        k=k-1;
+        while(true)
         {
-            if(!freq[i])
+            s = s + to_string(nums[k/fact]);
+            nums.erase(nums.begin()+k/fact);
+            if(nums.size()==0)
             {
-                arr.push_back(nums[i]);
-                freq[i]=1;
-                solve(ind+1,nums,arr,count,n,result,k,freq);
-                freq[i]=0;
-                arr.pop_back();
+                break; //ts is how while loop ends or terminates
             }
+            k=k%fact;
+            fact=fact/nums.size();
         }
+        return s;
     }
 };
